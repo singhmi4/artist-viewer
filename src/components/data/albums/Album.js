b30db1d7ac7d27b7249eca9d4cede3405d7df531
Michael Singh <singh.michaelanthony@gmail.com>
2020-12-29 15:10:31 -0500

    * create favAlbum state variables in context to be passed down to Album component * albums can be added to favourite list by clicking on heart icon in album card

--- src/components/data/albums/Album.js
+++ src/components/data/albums/Album.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+
+// Context Provider
+import MusixMatchProvider from '../../../context/context' 
 
 // Material UI Component
 import { 
   Card,
   CardContent,
   CardActions,
+  IconButton,
   Button,
   Typography
 } from '@material-ui/core'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles({
@@ -19,7 +25,32 @@ const useStyles = makeStyles({
 const Album = (props) => {
   const classes = useStyles();
   const { album } = props;
-  console.log(album);
+  const { favAlbums, setFavAlbums } = useContext(MusixMatchProvider.context)
+  const [fav, setFav] = useState(false)
+  console.log(album)
+  
+  // Checks if Album is in Fav Albums List to Display Appropriate Heart Icon
+  useEffect(() => {
+    const favAlbumFound = favAlbums.filter(item => item.album_id === album.album_id)
+    if (favAlbumFound.length) {
+      setFav(true)
+    } else {
+      setFav(false)
+    }
+  }, [])
+
+  const toggleFavourite = () => {
+    // Removes item from favourites list array
+    if (fav === true) {
+      setFav(false)
+      setFavAlbums(favAlbums.filter(item => item.album_id !== album.album_id))
+    // Adds item to favourites list array
+    } else {
+      setFav(true)
+      setFavAlbums([...favAlbums, album])
+    }
+  }
+
   return (
     <Card>
       <CardContent>
@@ -30,6 +61,13 @@ const Album = (props) => {
         </Typography>
         <Typography variant="body" component="p" gutterBottom><strong>Release Date:</strong> {album.album_release_date}</Typography>
       </CardContent>
+      <CardActions>
+        <IconButton onClick={toggleFavourite} >
+          {
+            fav === true ? <FavoriteIcon color="secondary" /> :  <FavoriteBorderIcon  /> 
+          }
+        </IconButton>
+      </CardActions>
     </Card>
   )
 }
