c4a0de2e09f0e587b98a720417766e02b0972b44
Michael Singh <singh.michaelanthony@gmail.com>
2020-12-29 15:39:54 -0500

    add snackbar message when album added to or removed from favourites

--- src/components/data/albums/Album.js
+++ src/components/data/albums/Album.js
@@ -9,11 +9,17 @@ import {
   CardContent,
   CardActions,
   IconButton,
-  Typography
+  Typography,
+  Snackbar
 } from '@material-ui/core'
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import FavoriteIcon from '@material-ui/icons/Favorite'
 import { makeStyles } from '@material-ui/core/styles'
+import MuiAlert from '@material-ui/lab/Alert'
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />
+}
 
 const useStyles = makeStyles({
   title: {
@@ -26,6 +32,7 @@ const Album = (props) => {
   const { album } = props;
   const { favAlbums, setFavAlbums } = useContext(MusixMatchProvider.context)
   const [fav, setFav] = useState(false)
+  const [open, setOpen] = useState(false);
   
   // Checks if Album is in Fav Albums List to Display Appropriate Heart Icon
   useEffect(() => {
@@ -39,6 +46,8 @@ const Album = (props) => {
 
   const toggleFavourite = () => {
     // Removes item from favourites list array
+    setOpen(true)
+    
     if (fav === true) {
       setFav(false)
       setFavAlbums(favAlbums.filter(item => item.album_id !== album.album_id))
@@ -49,15 +58,23 @@ const Album = (props) => {
     }
   }
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  }
+
   return (
     <Card>
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>Album</Typography>
         <Typography variant="h6">{album.album_name}</Typography>
-        <Typography variant="body" gutterBottom>
+        <Typography variant="body1" gutterBottom>
           <strong>Genre:</strong> {album.primary_genres.music_genre_list[0] !== undefined ? album.primary_genres.music_genre_list[0].music_genre.music_genre_name : "Music"}
         </Typography>
-        <Typography variant="body" component="p" gutterBottom><strong>Release Date:</strong> {album.album_release_date}</Typography>
+        <Typography variant="body1" component="p" gutterBottom><strong>Release Date:</strong> {album.album_release_date}</Typography>
       </CardContent>
       <CardActions>
         <IconButton onClick={toggleFavourite} >
@@ -65,6 +82,13 @@ const Album = (props) => {
             fav === true ? <FavoriteIcon color="secondary" /> :  <FavoriteBorderIcon  /> 
           }
         </IconButton>
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success">
+          {
+            fav === true ? "Album added to your favourites!" :  "Album removed from your favourites!"
+          }
+        </Alert>
+      </Snackbar>
       </CardActions>
     </Card>
   )
